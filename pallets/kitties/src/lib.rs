1c8a0ac8edf213f8c4b23ce51a6316f5fd94b23d
imanuvi <inv484@gmail.com>
2021-06-05 13:27:42 +0700

    checkpoint until exercise 5

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -2,11 +2,12 @@
 
 use codec::{Encode, Decode};
 use frame_support::{
-	decl_module, decl_storage, decl_event, decl_error, ensure, StorageValue, StorageDoubleMap,
+	decl_module, decl_storage, decl_event, decl_error, ensure, StorageValue, StorageDoubleMap, Parameter,
 	traits::Randomness, RuntimeDebug, dispatch::{DispatchError, DispatchResult},
 };
 use sp_io::hashing::blake2_128;
 use frame_system::ensure_signed;
+use sp_runtime::traits::{AtLeast32BitUnsigned, Bounded, One, CheckedAdd};
 
 #[cfg(test)]
 mod tests;
@@ -32,25 +33,30 @@ impl Kitty {
 
 pub trait Config: frame_system::Config {
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+	type Randomness: Randomness<Self::Hash>;
+	type KittyIndex: Parameter + AtLeast32BitUnsigned + Bounded + Default + Copy;
 }
 
 decl_storage! {
 	trait Store for Module<T: Config> as Kitties {
 		/// Stores all the kitties, key is the kitty id
-		pub Kitties get(fn kitties): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) u32 => Option<Kitty>;
+		pub Kitties get(fn kitties): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) T::KittyIndex => Option<Kitty>;
 		/// Stores the next kitty ID
-		pub NextKittyId get(fn next_kitty_id): u32;
+		pub NextKittyId get(fn next_kitty_id): T::KittyIndex;
 	}
 }
 
 decl_event! {
 	pub enum Event<T> where
 		<T as frame_system::Config>::AccountId,
+		<T as Config>::KittyIndex,
 	{
 		/// A kitty is created. \[owner, kitty_id, kitty\]
-		KittyCreated(AccountId, u32, Kitty),
+		KittyCreated(AccountId, KittyIndex, Kitty),
 		/// A new kitten is bred. \[owner, kitty_id, kitty\]
-		KittyBred(AccountId, u32, Kitty),
+		KittyBred(AccountId, KittyIndex, Kitty),
+		/// A kitty is transferred. \[from, to, kitty_id\]
+		KittyTransferred(AccountId, AccountId, KittyIndex),
 	}
 }
 
@@ -73,35 +79,21 @@ decl_module! {
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 
-			// TODO: refactor this method to use
-			// `Self::random_value` and `Self::get_next_kitty_id`
-			// to simplify the implementation
+			let kitty_id = Self::get_next_kitty_id()?;
 
-			NextKittyId::try_mutate(|next_id| -> DispatchResult {
-				let current_id = *next_id;
-				*next_id = next_id.checked_add(1).ok_or(Error::<T>::KittiesIdOverflow)?;
+			let dna = Self::random_value(&sender);
 
-				let payload = (
-					<pallet_randomness_collective_flip::Module<T> as Randomness<T::Hash>>::random_seed(),
-					&sender,
-					<frame_system::Module<T>>::extrinsic_index(),
-				);
-				let dna = payload.using_encoded(blake2_128);
+			// Create and store kitty
+			let kitty = Kitty(dna);
+			Kitties::<T>::insert(&sender, kitty_id, &kitty);
 
-				// Create and store kitty
-				let kitty = Kitty(dna);
-				Kitties::<T>::insert(&sender, current_id, kitty.clone());
-
-				// Emit event
-				Self::deposit_event(RawEvent::KittyCreated(sender, current_id, kitty));
-
-				Ok(())
-			})?;
+			// Emit event
+			Self::deposit_event(RawEvent::KittyCreated(sender, kitty_id, kitty));
 		}
 
 		/// Breed kitties
 		#[weight = 1000]
-		pub fn breed(origin, kitty_id_1: u32, kitty_id_2: u32) {
+		pub fn breed(origin, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) {
 			let sender = ensure_signed(origin)?;
 			let kitty1 = Self::kitties(&sender, kitty_id_1).ok_or(Error::<T>::InvalidKittyId)?;
 			let kitty2 = Self::kitties(&sender, kitty_id_2).ok_or(Error::<T>::InvalidKittyId)?;
@@ -127,32 +119,56 @@ decl_module! {
 
 			Self::deposit_event(RawEvent::KittyBred(sender, kitty_id, new_kitty));
 		}
+
+		/// Transfer a kitty to new owner
+		#[weight = 1000]
+		pub fn transfer(origin, to: T::AccountId, kitty_id: T::KittyIndex) {
+			let sender = ensure_signed(origin)?;
+
+			Kitties::<T>::try_mutate_exists(sender.clone(), kitty_id, |kitty| -> DispatchResult {
+				if sender == to {
+					ensure!(kitty.is_some(), Error::<T>::InvalidKittyId);
+					return Ok(());
+				}
+
+				let kitty = kitty.take().ok_or(Error::<T>::InvalidKittyId)?;
+
+				Kitties::<T>::insert(&to, kitty_id, kitty);
+
+				Self::deposit_event(RawEvent::KittyTransferred(sender, to, kitty_id));
+				
+				Ok(())
+			})?;
+		}
+
+		// // Set price
+		// pub fn set_price(origin) {
+
+		// }
+
+		// Buy 
 	}
 }
 
-pub fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
-	// TODO: finish this implementation
-	// selector[bit_index] == 0 -> use dna1[bit_index]
-	// selector[bit_index] == 1 -> use dna2[bit_index]
-	// e.g.
-	// selector = 0b00000001
-	// dna1		= 0b10101010
-	// dna2		= 0b00001111
-	// result	= 0b10101011
-	0
+fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
+	(!selector & dna1) | (selector & dna2)
 }
 
 impl<T: Config> Module<T> {
-	fn get_next_kitty_id() -> sp_std::result::Result<u32, DispatchError> {
-		NextKittyId::try_mutate(|next_id| -> sp_std::result::Result<u32, DispatchError> {
+	fn get_next_kitty_id() -> sp_std::result::Result<T::KittyIndex, DispatchError> {
+		NextKittyId::<T>::try_mutate(|next_id| -> sp_std::result::Result<T::KittyIndex, DispatchError> {
 			let current_id = *next_id;
-			*next_id = next_id.checked_add(1).ok_or(Error::<T>::KittiesIdOverflow)?;
+			*next_id = next_id.checked_add(&One::one()).ok_or(Error::<T>::KittiesIdOverflow)?;
 			Ok(current_id)
 		})
 	}
 
 	fn random_value(sender: &T::AccountId) -> [u8; 16] {
-		// TODO: finish this implementation
-		Default::default()
+		let payload = (
+			T::Randomness::random_seed(),
+			&sender,
+			<frame_system::Module<T>>::extrinsic_index(),
+		);
+		payload.using_encoded(blake2_128)
 	}
 }
--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,6 +1,8 @@
+#[cfg(test)]
 use super::*;
 
 use crate as kitties;
+use std::cell::RefCell;
 use sp_core::H256;
 use frame_support::{parameter_types, assert_ok, assert_noop};
 use sp_runtime::{
@@ -52,19 +54,36 @@ impl frame_system::Config for Test {
 	type SS58Prefix = SS58Prefix;
 }
 
+thread_local! {
+    static RANDOM_PAYLOAD: RefCell<H256> = RefCell::new(Default::default());
+}
+pub struct MockRandom;
+
+impl Randomness<H256> for MockRandom {
+    fn random(_subject: &[u8]) -> H256 {
+        RANDOM_PAYLOAD.with(|v| *v.borrow())
+    }
+}
+
+fn set_random(val: H256) {
+    RANDOM_PAYLOAD.with(|v| *v.borrow_mut() = val)
+}
+
 impl Config for Test {
-	type Event = Event;
+    type Event = Event;
+    type Randomness = MockRandom;
+    type KittyIndex = u32;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let mut t: sp_io::TestExternalities = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
-    t.execute_with(|| System::set_block_number(1) );
+	let mut t: sp_io::TestExternalities = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
+	t.execute_with(|| System::set_block_number(1) );
     t
 }
 
 fn last_event() -> Event {
-    System::events().last().unwrap().event.clone()
+	System::events().last().unwrap().event.clone()
 }
 
 #[test]
@@ -83,36 +102,68 @@ fn can_create() {
 
 #[test]
 fn gender() {
-    assert_eq!(Kitty([0; 16]).gender(), KittyGender::Male);
-    assert_eq!(Kitty([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).gender(), KittyGender::Female);
+	assert_eq!(Kitty([0; 16]).gender(), KittyGender::Male);
+	assert_eq!(Kitty([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).gender(), KittyGender::Female);
 }
 
 #[test]
 fn can_breed() {
-    new_test_ext().execute_with(|| {
-        assert_ok!(KittiesModule::create(Origin::signed(100)));
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(100)));
+		
+		set_random(H256::from([2; 32]));
 
-        System::set_extrinsic_index(1);
-
-        assert_ok!(KittiesModule::create(Origin::signed(100)));
+		assert_ok!(KittiesModule::create(Origin::signed(100)));
 
         assert_noop!(KittiesModule::breed(Origin::signed(100), 0, 11), Error::<Test>::InvalidKittyId);
         assert_noop!(KittiesModule::breed(Origin::signed(100), 0, 0), Error::<Test>::SameGender);
-        assert_noop!(KittiesModule::breed(Origin::signed(101), 0, 1), Error::<Test>::InvalidKittyId);
+		assert_noop!(KittiesModule::breed(Origin::signed(101), 0, 1), Error::<Test>::InvalidKittyId);
 
-        assert_ok!(KittiesModule::breed(Origin::signed(100), 0, 1));
+		assert_ok!(KittiesModule::breed(Origin::signed(100), 0, 1));
 
-        let kitty = Kitty([59, 254, 219, 122, 245, 239, 191, 125, 255, 239, 247, 247, 251, 239, 247, 254]);
+		let kitty = Kitty([187, 250, 235, 118, 211, 247, 237, 253, 187, 239, 191, 185, 239, 171, 211, 122]);
 
-        assert_eq!(KittiesModule::kitties(100, 2), Some(kitty.clone()));
+		assert_eq!(KittiesModule::kitties(100, 2), Some(kitty.clone()));
         assert_eq!(KittiesModule::next_kitty_id(), 3);
 
         assert_eq!(last_event(), Event::kitties(crate::Event::<Test>::KittyBred(100u64, 2u32, kitty)));
-    });
+	});
 }
 
 #[test]
-fn combine_dna_works() {
-	assert_eq!(combine_dna(0b11111111, 0b00000000, 0b00001111), 0b11110000);
-	assert_eq!(combine_dna(0b10101010, 0b11110000, 0b11001100), 0b11100010);
+fn can_transfer() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(100)));
+
+		assert_noop!(KittiesModule::transfer(Origin::signed(101), 200, 0), Error::<Test>::InvalidKittyId);
+
+		assert_ok!(KittiesModule::transfer(Origin::signed(100), 200, 0));
+
+		let kitty = Kitty([59, 250, 138, 82, 209, 39, 141, 109, 163, 238, 183, 145, 235, 168, 18, 122]);
+
+		assert_eq!(KittiesModule::kitties(200, 0), Some(kitty));
+        assert_eq!(Kitties::<Test>::contains_key(100, 0), false);
+
+		assert_eq!(last_event(), Event::kitties(RawEvent::KittyTransferred(100, 200, 0)));
+	});
+}
+
+#[test]
+fn handle_transfer() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(100)));
+			
+		System::reset_events();
+
+		assert_noop!(KittiesModule::transfer(Origin::signed(100), 100, 1), Error::<Test>::InvalidKittyId);
+
+		assert_ok!(KittiesModule::transfer(Origin::signed(100), 100, 0));
+
+		let kitty = Kitty([59, 250, 138, 82, 209, 39, 141, 109, 163, 238, 183, 145, 235, 168, 18, 122]);
+
+		assert_eq!(KittiesModule::kitties(100, 0), Some(kitty));
+
+        // no transfer event because no actual transfer is executed
+        assert_eq!(System::events().len(), 0);
+	});
 }
